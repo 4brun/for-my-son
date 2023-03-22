1bd22f0dfde594018b1ae3a0d75a9798f310f045
Dmitriy <dabrun57@gmail.com>
2023-03-22 19:36:56 +0300

    3D scroll

--- js/app.js
+++ js/app.js
@@ -1,11 +1,22 @@
-const zSpacing = -1000
-const lastPosition = zSpacing / 5
+const zSpacing = -1000;
+const frames = Array.from(document.getElementsByClassName("frame"));
+let lastPositin = zSpacing / 5;
+let zValues = [];
 
-const $frames = document.getElementsByClassName('frame')
-const frame = Array.from($frames)
-const zValues = []
+window.onscroll = function () {
+   let top = document.documentElement.scrollTop,
+      delta = lastPositin - top;
 
-window.onscroll = function() {
-   const top = document.documentElement.scrollTop
-   const delta = lastPosition - top
-}
+   lastPositin = top;
+
+   frames.forEach(function (n, i) {
+      zValues.push(i * zSpacing + zSpacing);
+      zValues[i] += delta * -5.5;
+      let frame = frames[i];
+      let transform = `translateZ(${zValues[i]}px)`;
+      let opacity = zValues[i] < Math.abs(zSpacing) / 1.8 ? 1 : 0
+       frame.setAttribute("style", `transform: ${transform}; opacity: ${opacity}`);
+   });
+};
+
+window.scrollTo(0, 1);
